b8c80ba9c0d1993c314aa9f09e00c72886361e52
Iaroslav Sedrichev <iasedric@microsoft.com>
2020-06-26 09:48:54 -0700

    first commit

---
+++ Tweet_sentiment.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 16:09:57 2020
+
+@author: iasedric
+"""
+
+import nltk                                # Python library for NLP
+from nltk.corpus import twitter_samples    # sample Twitter dataset from NLTK
+from nltk.corpus import stopwords          # module for stop words that come with NLTK
+from nltk.stem import PorterStemmer        # module for stemming
+from nltk.tokenize import TweetTokenizer   # module for tokenizing strings
+
+import matplotlib.pyplot as plt            # library for visualization
+import random                              # pseudo-random number generator
+import re                                  # library for regular expression operations
+import string                              # for string operations
+import numpy as np                           # library for scientific computing and matrix operations
+from os import getcwd
+import pandas as pd  
+
+# downloads sample twitter dataset. uncomment the line below if running on a local machine.
+nltk.download('twitter_samples')
+nltk.download('stopwords')
+
+# select the set of positive and negative tweets
+all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+
+tweets = all_positive_tweets + all_negative_tweets
+
+# split the data into two pieces, one for training and one for testing (validation set) 
+train_pos  = all_positive_tweets[:4000]
+train_neg  = all_negative_tweets[:4000]
+
+train_x = train_pos + train_neg 
+
+print("Number of tweets: ", len(train_x))
+
+# make a numpy array representing labels of the tweets
+labels = np.append(np.ones((len(all_positive_tweets))), np.zeros((len(all_negative_tweets))))
+
+#Import the english stop words list from NLTK
+stopwords_english = stopwords.words('english') 
+
+# Our selected sample. Complex enough to exemplify each step
+tweet = all_positive_tweets[2277]
+print(tweet)
+
+def process_tweet(tweet):
+    
+    # remove old style retweet text "RT"
+    tweet2 = re.sub(r'^RT[\s]+', '', tweet)
+
+    # remove hyperlinks
+    tweet2 = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet2)
+
+    # remove hashtags
+    # only removing the hash # sign from the word
+    tweet2 = re.sub(r'#', '', tweet2)
+
+    # instantiate tokenizer class
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+                                   reduce_len=True)
+    # tokenize tweets
+    tweet_tokens = tokenizer.tokenize(tweet2)
+
+    tweets_clean = []
+
+    for word in tweet_tokens: # Go through every word in your tokens list
+        if (word not in stopwords_english and  # remove stopwords
+            word not in string.punctuation):  # remove punctuation
+            tweets_clean.append(word)
+
+    # Instantiate stemming class
+    stemmer = PorterStemmer() 
+
+    # Create an empty list to store the stems
+    tweets_stem = [] 
+
+    for word in tweets_clean:
+        stem_word = stemmer.stem(word)  # stemming word
+        tweets_stem.append(stem_word)  # append to the list
+        
+        
+    return tweets_stem
+
+
+def build_freqs(tweets, ys):
+    """Build frequencies.
+    Input:
+        tweets: a list of tweets
+        ys: an m x 1 array with the sentiment label of each tweet
+            (either 0 or 1)
+    Output:
+        freqs: a dictionary mapping each (word, sentiment) pair to its
+        frequency
+    """
+    # Convert np array to list since zip needs an iterable.
+    # The squeeze is necessary or the list ends up with one element.
+    # Also note that this is just a NOP if ys is already a list.
+    yslist = np.squeeze(ys).tolist()
+
+    # Start with an empty dictionary and populate it by looping over all tweets
+    # and over all processed words in each tweet.
+    freqs = {}
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, y)
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] = 1    
+    return freqs
+
+def sigmoid(z): 
+    '''
+    Input:
+        z: is the input (can be a scalar or an array)
+    Output:
+        h: the sigmoid of z
+    '''
+    # calculate the sigmoid of z
+    h = 1/(1+np.exp(-1*z))
+    
+    return h
+
+
+def gradientDescent(x, y, theta, alpha, num_iters):
+    '''
+    Input:
+        x: matrix of features which is (m,n+1)
+        y: corresponding labels of the input matrix x, dimensions (m,1)
+        theta: weight vector of dimension (n+1,1)
+        alpha: learning rate
+        num_iters: number of iterations you want to train your model for
+    Output:
+        J: the final cost
+        theta: your final weight vector
+    Hint: you might want to print the cost to make sure that it is going down.
+    '''
+    ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
+    # get 'm', the number of rows in matrix x
+    m = len(x)
+    
+    for i in range(0, num_iters):
+        
+        # get z, the dot product of x and theta
+        z = np.dot(x,theta)
+        
+        # get the sigmoid of z
+        h = sigmoid(z)
+        
+        # calculate the cost function
+        J = (-1/m)*np.sum(y*np.log(h)+(1-y)*np.log(1-h))
+
+        # update the weights theta
+        theta = theta - float(alpha/m)*(np.dot(np.transpose(x),(h-y)))
+        
+    ### END CODE HERE ###
+    J = float(J)
+    return J, theta
+
+
+# choose the same tweet
+tweet = all_positive_tweets[2277]
+
+
+
+# call the imported function
+tweets_stem = process_tweet(tweet); # Preprocess a given tweet
+
+print('preprocessed tweet:')
+print(tweets_stem) # Print the result
+
+freqs = build_freqs(tweets, labels)
+
+# select some words to appear in the report. we will assume that each word is unique (i.e. no duplicates)
+keys = ['happi', 'merri', 'nice', 'good', 'bad', 'sad', 'mad', 'best', 'pretti',
+        '❤', ':)', ':(', '😒', '😬', '😄', '😍', '♛',
+        'song', 'idea', 'power', 'play', 'magnific']
+
+# list representing our table of word counts.
+# each element consist of a sublist with this pattern: [<word>, <positive_count>, <negative_count>]
+data = []
+
+# loop through our selected words
+for word in keys:
+    
+    # initialize positive and negative counts
+    pos = 0
+    neg = 0
+    
+    # retrieve number of positive counts
+    if (word, 1) in freqs:
+        pos = freqs[(word, 1)]
+        
+    # retrieve number of negative counts
+    if (word, 0) in freqs:
+        neg = freqs[(word, 0)]
+        
+    # append the word counts to the table
+    data.append([word, pos, neg])
+    
+data
+
+fig, ax = plt.subplots(figsize = (8, 8))
+
+# convert positive raw counts to logarithmic scale. we add 1 to avoid log(0)
+x = np.log([x[1] + 1 for x in data])  
+
+# do the same for the negative counts
+y = np.log([x[2] + 1 for x in data]) 
+
+# Plot a dot for each pair of words
+ax.scatter(x, y)  
+
+# assign axis labels
+plt.xlabel("Log Positive count")
+plt.ylabel("Log Negative count")
+
+# Add the word as the label at the same position as you added the points just before
+for i in range(0, len(data)):
+    ax.annotate(data[i][0], (x[i], y[i]), fontsize=12)
+
+ax.plot([0, 9], [0, 9], color = 'red') # Plot the red line that divides the 2 areas.
+plt.show()
+
+
+
+
+# Check the function
+# Construct a synthetic test case using numpy PRNG functions
+np.random.seed(1)
+# X input is 10 x 3 with ones for the bias terms
+tmp_X = np.append(np.ones((10, 1)), np.random.rand(10, 2) * 2000, axis=1)
+# Y Labels are 10 x 1
+tmp_Y = (np.random.rand(10, 1) > 0.35).astype(float)
+
+# Apply gradient descent
+tmp_J, tmp_theta = gradientDescent(tmp_X, tmp_Y, np.zeros((3, 1)), 1e-8, 700)
+print(f"The cost after training is {tmp_J:.8f}.")
+print(f"The resulting vector of weights is {[round(t, 8) for t in np.squeeze(tmp_theta)]}")
